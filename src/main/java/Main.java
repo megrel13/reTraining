454a74c823572495285ae65a1582abb7dd3a5e99
George Chanturia <g-o-g-i-@mail.ru>
2020-08-11 18:44:00 +0300

    1) Почему не могу воспльзоваться методами на имени list,хотя на имени созданного объекта могу. 2) В методах через цикл for бегаем по объектам списка militaryMans, он подсвечивается красным. не понятен этот момент.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main extends Human implements Walkable,Singable {
+public class Main extends Man {
+
     public Main(String name, String surname, int age) {
         super(name, surname, age);
     }
-    static List<Human> militaryMans = new ArrayList<>();
-    static Man gosha = new Man("Гоша", "Чантурия", 26);
-    static Man vitaliya = new Man("Виталя", "Нежинский", 29);
-    static Man george = new Man("Гоша", "Купчинский", 17);
-    Man petr = new Man("Петр", "Ярчер", 21);
 
     public static void main(String[] args) {
+        List<Man> militaryMans = new ArrayList<>();
+        Man gosha = new Man("Гоша", "Чантурия", 26);
+        Man vitaliya = new Man("Виталя", "Нежинский", 29);
+        Man george = new Man("Гоша", "Купчинский", 17);
+        Man petr = new Man("Петр", "Ярчер", 21);
         militaryMans.add(gosha);
         militaryMans.add(vitaliya);
         militaryMans.add(george);
-
-        for (Human man : militaryMans) {
-            if (man.getAge() > 17 && man.getAge() < 27) {
-                System.out.println(man);
-            }
-        }
-
+        militaryMans.add(petr);
     }
-
-    @Override
-    public void walk() {
-        for (Human hum : militaryMans)
-        if (getName() == "Гоша" && getSurname() == "Чантурия") {
-            System.out.println("Гоша бегает!");
-        }
-    }
-
 }
+
+
--- src/main/java/Man.java
+++ src/main/java/Man.java
@@ -1,8 +1,29 @@
-public class Man extends Human{
-    public Man(String name, String surname, int age) {
-        super(name, surname, age);
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class Man implements Walkable, Singable {
+    private final String name;
+    private final String surname;
+    private int age;
+
+    @Override
+    public void walk() {
+        for (Man man : militaryMans)
+            if (getName() == "Гоша" && getSurname() == "Чантурия") {
+                System.out.println("Гоша бегает!");
+            }
     }
 
-
-
+    public void printWhoGoToArmy() {
+        for (Man man : militaryMans) {
+            if (man.getAge() > 17 && man.getAge() < 27) {
+                System.out.println(man);
+            }
+        }
     }
+}
